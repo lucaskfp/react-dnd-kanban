cf1eb74358b928490bbf4a287815793c7ef2244f
Lucas Kennedy <lukakfp@gmail.com>
2024-10-14 13:47:29 -0300

    Update Tailwind CSS configuration to add a new primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: ({colors}) => ({
+        'primary': colors.red[500],
+      })
+    },
   },
   plugins: [],
 }
